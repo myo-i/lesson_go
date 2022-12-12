b023b5bd0e4338c41e89ded7061595179b2028b8
myo-i <Imiyo0521@gmail.com>
2022-12-12 23:42:43 +0900

    🚑 ✨ godoc（何故かサーバーが動かなかったから要復習）

--- mylib/math.go
+++ mylib/math.go
@@ -1,5 +1,9 @@
+/*
+mylib is homemade library.
+*/
 package mylib
 
+// Average returns the average of series of numbers
 func Average(s []int) int {
 	total := 0
 	for _, i := range s {
--- mylib/math_test.go
+++ mylib/math_test.go
@@ -1,8 +1,17 @@
 package mylib
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 var debug bool = true
+
+// Example~ でパッケージの使用例を記載できる
+func ExampleAverage() {
+	v := Average([]int{1, 2, 3, 4, 5, 6, 7})
+	fmt.Println(v)
+}
 
 func TestAverage(t *testing.T) {
 	if debug {
