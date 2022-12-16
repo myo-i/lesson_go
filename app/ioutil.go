aa798416f47e3c4f945ee6a2f2a879eb93e20b24
myo-i <Imiyo0521@gmail.com>
2022-12-17 03:48:40 +0900

    ✨テンプレートをいちいち呼び出さないように、1つのハンドラーから値に応じて異なるハンドラーを呼ぶ処理

--- app/ioutil.go
+++ app/ioutil.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"regexp"
 )
 
 type Page struct {
@@ -28,16 +29,21 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+// 既に開いた状態にしておいて必要な時に呼び出していく
+var templates = template.Must(template.ParseFiles("app/"+"edit.html", "app/"+"view.html"))
+
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, _ := template.ParseFiles("app/" + tmpl + ".html")
-	t.Execute(w, p)
+	//t, _ := template.ParseFiles("app/" + tmpl + ".html")
+	//t.Execute(w, p)
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 // RequestはURLで打ち込んだ/view/testの部分のこと
 // ResponseWriterはレスポンス
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	// /view/testを受け取ったらtestを抽出
-	title := r.URL.Path[len("/view/"):]
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 	// test.txtを読み込んでその中身を返している
 	p, err := loadPage(title)
 	if err != nil {
@@ -47,8 +53,7 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "view", p)
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
@@ -56,8 +61,7 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "edit", p)
 }
 
-func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/save/"):]
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	// edit.htmlにname="body"があるのでそこから取得
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
@@ -70,12 +74,27 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+// わかりにくい...
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		// 下記のfnがsaveHandlerとかeditHandlerのこと
+		fn(w, r, m[2])
+	}
+}
+
 func Ioutil() {
 	// /view/を受け取ったらviewHandlerへ飛ばす
-	// Handlefuncの第二引数はfunc(ResponseWriter, Request)（定義みろ！！）
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/save/", saveHandler)
+	// Handlefuncの第二引数はfunc(ResponseWriter, Request)しか受け付けない（定義みろ！！）
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
 	// Handlerを登録したいのであれば以下を実行してサーバーを立てる前に上記のようにハンドラーを登録する必要がある
 	// 上記のハンドラーが登録出来たら、以下のメソッドでレスポンスを返す（サーバーも起動）
 	log.Fatal(http.ListenAndServe(":8080", nil))
