c9141f156bc7bcfafabbc89b083f5968c41de3b0
myo-i <Imiyo0521@gmail.com>
2022-12-16 23:31:34 +0900

    ✨ http.ListenAndServe（httpからリクエストを受け取って、レスポンスを返す）

--- app/ioutil.go
+++ app/ioutil.go
@@ -3,6 +3,8 @@ package app
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 type Page struct {
@@ -26,10 +28,21 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
-func Ioutil() {
-	p1 := &Page{Title: "test", Body: []byte("Sample Data")}
-	p1.save()
+// RequestはURLで打ち込んだ/view/testの部分のこと
+// ResponseWriterはレスポンス
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	// /view/testを受け取ったらtestを抽出
+	title := r.URL.Path[len("/view/"):]
+	// test.txtを読み込んでその中身を返している
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
 
-	p2, _ := loadPage(p1.Title)
-	fmt.Sprintf(string(p2.Body))
+func Ioutil() {
+	// /view/を受け取ったらviewHandlerへ飛ばす
+	// Handlefuncの第二引数はfunc(ResponseWriter, Request)（定義みろ！！）
+	http.HandleFunc("/view/", viewHandler)
+	// Handlerを登録したいのであれば以下を実行してサーバーを立てる前に上記のようにハンドラーを登録する必要がある
+	// 上記のハンドラーが登録出来たら、以下のメソッドでレスポンスを返す（サーバーも起動）
+	log.Fatalln(http.ListenAndServe(":8080", nil))
 }
